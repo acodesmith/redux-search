6403b83779062afe12d1c65463a0376da85e6584
acodesmith <adam@acodesmith.com>
2020-08-30 22:22:36 -0400

    removing files.

--- devtools.js
+++
@@ -1,8 +0,0 @@
-chrome.devtools.panels.create(
-  'Redux Search',
-  'img/logo/redux-search-logo-color.png',
-  'devpanel.html',
-  function (panel) {
-    // code invoked on panel creation
-  }
-);
