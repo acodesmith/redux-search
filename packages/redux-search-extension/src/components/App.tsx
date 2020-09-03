0287c49e32735b896cad38c10b360bab78fc2266
acodesmith <adam@acodesmith.com>
2020-09-02 23:50:33 -0400

    Using npm link to run new js-dive in the browser extension

--- packages/redux-search-extension/src/components/App.tsx
+++ packages/redux-search-extension/src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import JSDive from 'js-dive';
 import { Search } from './Search';
 import '../styles/global.scss';
+
+console.log({ JSDive });
 
 export const App: React.FC<{}> = () => {
   return (
     <div>
-      <Search />
+      <Search onSearch={(v: string) => {}} />
     </div>
   );
 };
